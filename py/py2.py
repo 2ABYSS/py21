8a38f98d0fcf6c77b2577905227dda4a81ffc64c
2ABYSS <155827797+2ABYSS@users.noreply.github.com>
2024-01-17 19:00:22 +0300

    Add files via upload

---
+++ py/py2.py
@@ -0,0 +1,37 @@
+# # dogru_mu=True
+# # yanlis_mi = False
+# # ondalik1= 3.14
+# # ondalik2= 0.5 
+# # sayi1= 42
+# # sayi2= -17 
+# # sayi3= 15.5
+
+# # sayi_float= float(sayi1)
+# # print("int to float", sayi_float)
+
+
+
+# # sayi_int= int (ondalik1)
+# # print("float to int", sayi_int)
+
+
+# # sayi_bool = int(dogru_mu)
+# # sayi_bool = int(yanlis_mi)
+# # print("bool to int", sayi_bool)
+# # print(type(sayi_bool))
+
+
+# # kullanici_ad = input("lutfen adinizi giriniz:")
+# # print("hosgeldin", kullanici_ad)
+
+
+
+# sayi1=int(input("birinci  sayi giriniz"))
+# sayi2=int(input("ikinci  sayi giriniz"))
+# print(sayi1+sayi2)
+
+# sayi3= input("1. sayiyi gir")
+# sayi4= input ("2. sayiyi gir")
+# print(int(sayi3)+int(sayi4))
+
+
---
+++ py/sibervatan.py
@@ -0,0 +1,30 @@
+sayi = 12
+print(sayi)
+
+sayi = "10"
+sayi = "100"
+print(sayi)
+
+
+
+us = pow(2,3)
+print(" sayi ussu", us )
+
+
+
+from math import sqrt,ceil,floor 
+sayi = 25 
+karekok= sqrt(sayi)
+print("sayinin karekoku", karekok)
+
+
+
+ondalik = 10.25
+yukari= ceil(ondalik)
+asagi = floor(ondalik)
+print("yukari", asagi)
+print("asagi", asagi)
+
+
+
+
